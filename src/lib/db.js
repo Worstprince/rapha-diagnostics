66b755bae673c5fac06f3b89a7a5bc112e63edc9
arkuv <arku890@gmail.com>
2026-07-13 04:40:26 +0800

    added try catch for db

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,17 @@
 import mysql from "mysql2/promise";
 
-const db = await mysql.createPool({
-    port: 3307,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "raphaschema",
-});
+let db;
+
+try {
+    db = await mysql.createPool({
+        port: 3307,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "raphaschema",
+    });
+} catch (error) {
+    console.error("Error connecting to the database:", error);
+}
 
 export default db;
